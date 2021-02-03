f04403a3a2aba54576144d5c5f06382eb2e7661b
D18130495 <d18130495@MYTUDUBLIN.IE>
2021-02-03 16:05:24 +0000

    update

---
+++ java/src/ie/tudublin/BugZap.java
@@ -0,0 +1,26 @@
+package ie.tudublin;
+
+import processing.core.PApplet;
+
+public class HelloProcessing2 extends PApplet
+{
+	public void settings(int x, int y)
+	{
+		size(x, y);
+	}
+
+    public void setup() 
+    {
+		
+	}
+
+	float x = 300;
+	int i;
+	String s;
+	boolean b;
+	
+	public void draw()
+	{	
+        settings(500 ,500);
+	}
+}

f40e0715a9b157b90ec143f17ad716d81c82c273
D18130495 <d18130495@MYTUDUBLIN.IE>
2021-02-24 14:52:18 +0000

    ,y lab5

--- java/src/ie/tudublin/Main.java
+++ java/src/ie/tudublin/Main.java
@@ -2,17 +2,17 @@ package ie.tudublin;
 
 public class Main
 {
+    public void arrays()
+    {
+        String[] a = {"MAIN"};
+        processing.core.PApplet.runSketch( a, new Arrays());
+    }
     public void loops()
     {
         String[] a = {"MAIN"};
         processing.core.PApplet.runSketch( a, new Loops());
     }
 
-    public void arrays()
-    {
-        String[] a = {"MAIN"};
-        processing.core.PApplet.runSketch( a, new Arrays());
-    }
     public void life()
     {
         String[] a = {"MAIN"};

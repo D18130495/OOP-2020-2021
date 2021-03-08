90ee2a792f787c39e47507ff549f75e1a295b58f
D18130495 <d18130495@MYTUDUBLIN.IE>
2021-03-08 18:19:40 +0000

    my lab5

--- java/src/ie/tudublin/Main.java
+++ java/src/ie/tudublin/Main.java
@@ -13,48 +13,15 @@ public class Main
         processing.core.PApplet.runSketch( a, new Loops());
     }
 
-<<<<<<< HEAD
 
-=======
-    public void arrays()
-    {
-        String[] a = {"MAIN"};
-        processing.core.PApplet.runSketch( a, new Arrays());
-    }
-    
->>>>>>> 1d929a5fde2c32c05586db81341eeb20de57dd28
     public void life()
     {
         String[] a = {"MAIN"};
-        processing.core.PApplet.runSketch( a, new Life1());
+        processing.core.PApplet.runSketch( a, new Life());
     }
-    
-    public void colorfulLife()
-    {
-        String[] a = {"MAIN"};
-        processing.core.PApplet.runSketch( a, new ColorfulLife());
-    }
-
-    public void starMap()
-    {
-        String[] a = {"MAIN"};
-        processing.core.PApplet.runSketch( a, new StarMap());
-    }
-    public void gantt()
-    {
-        String[] a = {"MAIN"};
-        processing.core.PApplet.runSketch( a, new Gantt());
-    }
-
-    public void audio1()
-    {
-        String[] a = {"MAIN"};
-        processing.core.PApplet.runSketch( a, new Audio1());
-    }
-
     public static void main(String[] args)
     {
         Main main = new Main();
-        main.audio1();
+        main.life();
     }
 } 

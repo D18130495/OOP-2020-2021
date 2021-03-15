10035cb0989590da3aecce60727c14d2e1243024
D18130495 <d18130495@MYTUDUBLIN.IE>
2021-03-15 14:13:43 +0000

    week7 class

--- java/src/ie/tudublin/Main.java
+++ java/src/ie/tudublin/Main.java
@@ -8,63 +8,9 @@ public class Main
         processing.core.PApplet.runSketch( a, new Audio1());
     }
 
-<<<<<<< HEAD
     public static void main(String[] args)
     {
         Main main = new Main();
         main.audio1();
-=======
-    public void loops()
-    {
-        String[] a = {"MAIN"};
-        processing.core.PApplet.runSketch( a, new Loops());
-    }
-
-    public void arrays()
-    {
-        String[] a = {"MAIN"};
-        processing.core.PApplet.runSketch( a, new Arrays());
-    }
-    
-    public void life()
-    {
-        String[] a = {"MAIN"};
-        processing.core.PApplet.runSketch( a, new Life1());
-    }
-    
-    public void colorfulLife()
-    {
-        String[] a = {"MAIN"};
-        processing.core.PApplet.runSketch( a, new ColorfulLife());
-    }
-
-    public void starMap()
-    {
-        String[] a = {"MAIN"};
-        processing.core.PApplet.runSketch( a, new StarMap());
-    }
-    public void gantt()
-    {
-        String[] a = {"MAIN"};
-        processing.core.PApplet.runSketch( a, new Gantt());
-    }
-
-    public void audio1()
-    {
-        String[] a = {"MAIN"};
-        processing.core.PApplet.runSketch( a, new Audio1());
-    }
-
-    public void audio2()
-    {
-        String[] a = {"MAIN"};
-        processing.core.PApplet.runSketch( a, new Audio2());
-    }
-
-    public static void main(String[] args)
-    {
-        Main main = new Main();
-        main.audio2();
->>>>>>> f0a0ce9fce1d3be7814c563e542b8b1ec31771a1
     }
 } 
